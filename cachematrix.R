9e86ef2cc058224f9619493cf2d6594b39e6bb6b
Greg Etling <greg@gregetling.com>
2015-08-11 12:42:40 -0400

    Solved

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,26 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  # Initialization, set(), and get() for object are identical to example
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  
+  # setinverse() is functionally identical to example
+  setinverse <- function(inverse) inv <<- inverse
+  
+  # getinverse() is functionally identical to example
+  getinverse <- function() inv
+  
+  # Return the object to the caller
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
 }
 
 
@@ -12,4 +31,23 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  # Get inverse from cache
+  inv <- x$getinverse()
+  
+  # If inverse exists in cache, return it
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  
+  # Inverse not set in cache, get the data from the object
+  data <- x$get()
+  
+  # Compute the inverse
+  inv <- solve(data, ...)
+  
+  # Cache the inverse and return it
+  x$setinverse(inv)
+  inv
 }
